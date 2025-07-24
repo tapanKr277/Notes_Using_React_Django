7649dec315683f24240b9b6bab926721d0c7e5c5
AlexCrew007 <tapankumar07081998@gmail.com>
2025-07-24 19:57:25 +0530

    static folder added

--- myNotes/myNotes/settings.py
+++ myNotes/myNotes/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True 
